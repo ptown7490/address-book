ddf7080d393ec6b3ad66209b2f2c0f70469834fa
Patrick McGreevy & Benjamin Paul Dunphy <student@epicodus.com>
2014-02-25 12:08:54 -0800

    add functional input with multiple addresses possible

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,79 @@ var Contact = {
   }
 };
 
+var Address = {
+  fullAddress: function() {
+    return this.street + "\n" + this.city + ", " + this.state + " " + this.zip;
+  }
+};
+
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                '<div class="form-group">' +
+                  '<label for="new-street">Street</label>' +
+                  '<input type="text" class="form-control" id="new-street">' +
+                '</div>' +
+                '<div class="form-group">' +
+                  '<label for="new-city">City</label>' +
+                  '<input type="text" class="form-control" id="new-city">' +
+                '</div>' +
+                '<div class="form-group">' +
+                  '<label for="new-state">State</label>' +
+                  '<input type="text" class="form-control" id="new-state">' +
+                '</div>' +
+                '<div class="form-group">' +
+                  '<label for="new-zip">Zip</label>' +
+                  '<input type="text" class="form-control" id="new-zip">' +
+                '</div>' +
+              '</div>');
+  });
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#first-name").val();
-    var inputtedLastName = $("input#last-name").val();
-    var inputtedAddress = $("input#address").val();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    
+
 
     var newContact = Object.create(Contact);
     newContact.firstName = inputtedFirstName;
     newContact.lastName = inputtedLastName;
-    newContact.address = inputtedAddress;
+
+    newContact.addresses = [];
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $("input.new-street").val();
+      var inputtedCity = $("input.new-city").val();
+      var inputtedState = $("input.new-state").val();
+      var inputtedZip = $("input.new-zip").val();
+
+      var newAddress = Object.create(Address);
+      newAddress.street = inputtedStreet;
+      newAddress.city = inputtedCity;
+      newAddress.state = inputtedState;
+      newAddress.zip = inputtedZip;
+
+      newContact.addresses.push(newAddress);
+    });
+
+
     $("ul#contacts").append("<li><span class=\"contact\">" + newContact.fullName() + "</span></li>");
-    this.reset();
+
+    
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
+
       $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
+ 
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() +"</li>");
+      });
     });
+
+    this.reset();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -9,3 +9,15 @@ describe("Contact", function() {
   });
 });
 
+describe("Address", function() {
+  describe("fullAddress", function() {
+    it("shows the street, city, state, and zip on the same line separated by commas", function() {
+      var testAddress = Object.create(Address);
+      testAddress.street = "123 Main St";
+      testAddress.city = "Portland";
+      testAddress.state = "OR";
+      testAddress.zip = "97201";
+      testAddress.fullAddress().should.equal("123 Main St\nPortland, OR 97201");
+    });
+  });
+});
