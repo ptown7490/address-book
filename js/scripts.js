691d9f89b450e6bd1b83bc1f28865c513e96b1d5
Patrick McGreevy & Benjamin Paul Dunphy <student@epicodus.com>
2014-02-25 16:39:23 -0800

    Add isValid conditional for address and phone parameters and Add phone numbers

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,68 @@ var Contact = {
 
 var Address = {
   fullAddress: function() {
-    return this.street + "\n" + this.city + ", " + this.state + " " + this.zip;
+    return this.street + "<br />" + this.city + ", " + this.state + " " + this.zip;
+  },
+
+  valid: function() {
+    if (this.street.search(/[0-9]/) == -1)
+      return false;
+    if (this.city.search(/[0-9]/) != -1)
+      return false;
+    if (this.state.length != 2 || this.state.search(/[0-9]/) != -1)
+      return false;
+    if (this.zip.length != 5 || this.zip.search(/[a-z]/i) != -1)
+      return false;
+
+    return true;
+  }
+
+};
+
+var Phone = {
+  formatted: function() {
+    return "(" + this.digits.slice(0,3) + ") " + this.digits.slice(3,6) + "-" + this.digits.slice(6);
+  },
+
+  valid: function() {
+    if (this.digits.length !== 10) {
+      return false;
+    }
+    return true;
   }
 };
+
+
 
 $(document).ready(function() {
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' +
-                '<div class="form-group">' +
-                  '<label for="new-street">Street</label>' +
-                  '<input type="text" class="form-control" id="new-street">' +
-                '</div>' +
-                '<div class="form-group">' +
-                  '<label for="new-city">City</label>' +
-                  '<input type="text" class="form-control" id="new-city">' +
-                '</div>' +
-                '<div class="form-group">' +
-                  '<label for="new-state">State</label>' +
-                  '<input type="text" class="form-control" id="new-state">' +
-                '</div>' +
-                '<div class="form-group">' +
-                  '<label for="new-zip">Zip</label>' +
-                  '<input type="text" class="form-control" id="new-zip">' +
-                '</div>' +
-              '</div>');
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control" id="new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control" id="new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control" id="new-state">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-zip">Zip</label>' +
+                                   '<input type="text" class="form-control" id="new-zip">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
+  $("#add-phone").click(function() {
+    $("#new-phones").append('<div class="new-phone">' +
+                              '<div class="form-group">' +
+                                '<label for="new-phone-in">Phone</label>' +
+                                '<input type="text" class="form-control" id="new-phone-in">' +
+                              '</div>' +
+                            '</div>');
   });
 
   $("form#new-contact").submit(function(event) {
@@ -45,12 +83,13 @@ $(document).ready(function() {
     newContact.lastName = inputtedLastName;
 
     newContact.addresses = [];
+    newContact.phones = [];
 
     $(".new-address").each(function() {
-      var inputtedStreet = $("input.new-street").val();
-      var inputtedCity = $("input.new-city").val();
-      var inputtedState = $("input.new-state").val();
-      var inputtedZip = $("input.new-zip").val();
+      var inputtedStreet = $(this).find("input#new-street").val();
+      var inputtedCity = $(this).find("input#new-city").val();
+      var inputtedState = $(this).find("input#new-state").val();
+      var inputtedZip = $(this).find("input#new-zip").val();
 
       var newAddress = Object.create(Address);
       newAddress.street = inputtedStreet;
@@ -61,6 +100,14 @@ $(document).ready(function() {
       newContact.addresses.push(newAddress);
     });
 
+    $(".new-phone").each(function() {
+      var inputtedPhone = $(this).find("input#new-phone-in").val();
+      
+      var newPhone = Object.create(Phone);
+      newPhone.digits = inputtedPhone;
+
+      newContact.phones.push(newPhone);
+    });
 
     $("ul#contacts").append("<li><span class=\"contact\">" + newContact.fullName() + "</span></li>");
 
@@ -73,7 +120,18 @@ $(document).ready(function() {
  
       $("ul#addresses").text("");
       newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() +"</li>");
+        if (address.valid())
+          $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+        else
+          $("ul#addresses").append("<li>Invalid Address Submitted</li>");
+      });
+
+      $("ul#phones").text("");
+      newContact.phones.forEach(function(phone) {
+        if (phone.valid())
+          $("ul#phones").append("<li>" + phone.formatted() + "</li>");
+        else
+          $("ul#phones").append("<li>Invalid phone number submitted</li>");
       });
     });
 
--- spec/specs.js
+++ spec/specs.js
@@ -17,7 +17,36 @@ describe("Address", function() {
       testAddress.city = "Portland";
       testAddress.state = "OR";
       testAddress.zip = "97201";
-      testAddress.fullAddress().should.equal("123 Main St\nPortland, OR 97201");
+      testAddress.fullAddress().should.equal("123 Main St<br />Portland, OR 97201");
+    });
+  });
+
+  describe("valid", function() {
+    it("returns false if an address is entered incorrectly", function() {
+      var testAddress = Object.create(Address);
+      testAddress.street = "Main St";
+      testAddress.city = "Portl8";
+      testAddress.state = "ORR";
+      testAddress.zip = "9720";
+      testAddress.valid().should.equal(false);      
+    });
+  });
+});
+
+describe("Phone", function() {
+  describe("formatted", function() {
+    it("returns a string of the 10-digit phone number in '(###) ###-####' format", function() {
+      var testPhone = Object.create(Phone);
+      testPhone.digits = "5035554444";
+      testPhone.formatted().should.equal("(503) 555-4444");
+    });
+  });
+
+  describe("valid", function() {
+    it("returns false if a phone number is entered incorrectly", function() {
+      var testPhone = Object.create(Phone);
+      testPhone.digits = "555666777";
+      testPhone.valid().should.equal(false);
     });
   });
 });
